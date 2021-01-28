176f0af89f1deb598fd54d5944c8990328503a44
Srinivas Madarapu <sri.nivas006@gmail.com>
2021-01-28 09:33:56 +0530

    Adding Tables(Sale, Product, SaleDetail, User,Inventory) to the database RMData  Project

--- RMData/dbo/Tables/User.sql
+++ RMData/dbo/Tables/User.sql
@@ -1,4 +1,9 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id]  NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getUTCDate(),
+
 )
